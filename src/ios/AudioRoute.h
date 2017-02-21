b7f0200941fff2f0f47671a7fac2efc4e40dd78c
Jay Mehta <jay@solutionanalysts.com>
2017-02-21 13:05:04 +0530

    Changed implementation to MPVolumeview.

--- src/ios/AudioRoute.h
+++ src/ios/AudioRoute.h
@@ -1,8 +1,10 @@
 #import <AVFoundation/AVFoundation.h>
 #import <Cordova/CDV.h>
+#import <MediaPlayer/MediaPlayer.h>
 
 @interface AudioRoute :CDVPlugin {
     NSString* callbackId;
+    MPVolumeView *mpVolumeView;
 }
 
 - (void) currentOutputs:(CDVInvokedUrlCommand*)command;
